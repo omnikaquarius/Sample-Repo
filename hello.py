afeeee5a621c0cb83ebfed94842a2134bf1f6267
Omnik Aquarius <47669240+omnikaquarius@users.noreply.github.com>
2019-09-12 19:56:00 -0300

    this is a initial commit

---
+++ hello.py
@@ -0,0 +1,49 @@
+# hello = "hello"
+# print(hello)
+
+# for i in range(4):
+#     if i > 0:
+#         print(i)
+
+# print("===============================")
+
+# input_peso = input("Peso em Pounds: ")
+# peso = int(input_peso)
+# resultado = peso / 2.2046
+# print(f"[-> o peso em Kilogramas é de {str(round(resultado,2))}]")
+
+# print(10 + 3 * 2 ** 2)
+
+# equacao = ""
+
+# operadores = "+ - * / **"
+# while 1 > 0:
+#         num = input("Número: ")
+#         op = "null"
+#         print(operadores.find(op))
+#         if operadores.find(op) == -1:
+#                 op = input("Operador ")
+
+#         equacao += f"{num} {op} "
+#         print(equacao)
+
+import math
+import sys
+from os import rename
+
+import requests
+
+print(sys.version)
+print(sys.executable)
+
+
+def greet(Who):
+    greeting = f"Hello, {Who}"
+    return greeting
+
+
+r = requests.get("https://coreyms.com")
+print(r.status_code)
+""" name = input("Your name? ")
+print(f"Hello, {name}!")
+ """
